8851bd202132f84b742280f70f71395e36c0aac1
Abbas Hameedgit config --global user.email abbashmeed@gmail.com <57520854+Abbas-Hameed@users.noreply.github.com>
2022-01-30 02:56:34 +0530

    trying to solve github prpblem

---
+++ TestGit1/TestGit1App.swift
@@ -0,0 +1,17 @@
+//
+//  TestGit1App.swift
+//  TestGit1
+//
+//  Created by Abbas Hameed on 27/01/22.
+//
+
+import SwiftUI
+
+@main
+struct TestGit1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
